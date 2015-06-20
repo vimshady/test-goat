e9c7c2da89dc58a3aa48a6fb8f6a93a0d748dfdd
osewe <eikheloa@gmail.com>
2015-06-19 20:03:49 -0400

    enforce model validation in list view

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -28,6 +28,5 @@ class ItemValidationTest(FunctionalTest):
         # And she can correct it by filling some text in
         self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
         self.check_for_row_in_list_table('1: Buy milk')
-        self.check_for_row_in_list_table('2: Make tea\n')
-        self.fail('write me!')
+        self.check_for_row_in_list_table('2: Make tea')
 
