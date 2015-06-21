ccdb962bd59b05a78d793d8c6c96aacf1ecb0dd2
osewe <eikheloa@gmail.com>
2015-06-20 22:43:00 -0400

    use form in all views, back to working state

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -50,7 +50,7 @@ class NewListTest(TestCase):
 
     def test_validation_errors_are_shown_on_home_page(self):
         response = self.client.post('/lists/new', data={'text': ''})
-        self.assertContains(response, EMPTY_ITEM_ERROR)
+        self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
 
     def test_for_invalid_input_passes_form_to_template(self):
@@ -131,5 +131,29 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/%d/' % (list_.id))
         self.assertIsInstance(response.context['form'], ItemForm)
         self.assertContains(response, 'name="text"')
+
+    def post_invalid_input(self):
+        list_ = List.objects.create()
+        return self.client.post(
+            '/lists/%d/' % (list_.id,),
+            data={'text': ''} 
+        )
+
+    def test_form_invalid_input_nothing_saved_to_db(self):
+        self.post_invalid_input()
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_for_invalid_input_renders_list_template(self):
+        response = self.post_invalid_input()
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.post_invalid_input()
+        self.assertIsInstance(response.context['form'], ItemForm)
+
+    def test_for_invalid_input_shows_error_on_page(self):
+        response = self.post_invalid_input()
+        self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
 
